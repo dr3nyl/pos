43d8798c0f7959315308d040c8206572981391a4
Drenyl <drenyltobi@gmail.com>
2022-07-05 01:18:57 +0800

    Change user dashboard header to Activity feed and justify center it

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('User Dashboard') }}
+        <h2 class="flex justify-center font-semibold text-2xl text-gray-800 leading-tight">
+            {{ __('Activity Feed') }}
         </h2>
     </x-slot>
 
